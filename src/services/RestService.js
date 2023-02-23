171e7058fe2dd736b86e144c06e9f6ae4b8aa609
J. Ivan Martinez Mateos <jimm0063@gmail.com>
2023-02-22 18:40:22 -0600

    - change url for prod environment

--- src/services/RestService.js
+++ src/services/RestService.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-//const url = "http://192.168.4.70:8081";
-const url = "http://localhost:8081";
+const url = "http://192.168.4.70:8081";
+//const url = "http://localhost:8081";
 
 const postRequest = (endpoint, body) => {
+    console.log(url);
     return axios.post(url + endpoint, body)
 }
 
 const getRequest = (endpoint) => {
+    console.log(url);
     return axios.get(url + endpoint)
 }
 
